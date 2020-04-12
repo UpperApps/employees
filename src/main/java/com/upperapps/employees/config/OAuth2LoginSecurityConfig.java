4a484dc8ab5fb5b15b36a18ad26f532b6165f74d
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2020-04-12 15:55:47 -0500

    Included new Spring Security Configuration spring-security-oauth2 library

--- src/main/java/com/upperapps/employees/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/upperapps/employees/config/OAuth2LoginSecurityConfig.java
@@ -1,13 +1,17 @@
 package com.upperapps.employees.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
-public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableResourceServer
+public class OAuth2LoginSecurityConfig extends ResourceServerConfigurerAdapter {
+    
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .anyRequest().authenticated();
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.antMatcher("/api/**").authorizeRequests().anyRequest().authenticated();
     }
 }
